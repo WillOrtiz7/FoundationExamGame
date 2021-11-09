c3bf8aa4514c5210e1da6ebce04ba4931475b727
WilsonOrtiz7 <wilsonortiz954@gmail.com>
2021-11-08 20:59:01 -0500

    Implemented basics (movement, camera, health)

---
+++ PlayerHealth.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int health;
+    public bool hasDied;
+    
+    void Start()
+    {
+        hasDied = false;
+    }
+
+    void Update()
+    {
+        if (gameObject.transform.position.y < -9)
+        {
+            Debug.Log("Player has died!");
+            hasDied = true;
+        }
+        if (hasDied == true)
+        {
+            StartCoroutine("Die");
+        }
+    }
+
+    IEnumerator Die()
+    {
+        SceneManager.LoadScene("Prototype1");
+        yield return null;
+    }
+}
---
+++ PlayerMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public int playerSpeed = 6;
+    private bool facingRight = true;
+    public int playerJumpPower = 1000;
+    private float moveX;
+    public float moveY;
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+    }
+
+    void PlayerMove()
+    {
+        //CONTROLS
+        moveX = Input.GetAxis("Horizontal");
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+        //ANIMATIONS
+        //PLAYER DIRECTION
+        if (moveX < 0.0f && facingRight == true)
+        {
+            FlipPlayer();
+        }
+        else if (moveX > 0.0f && facingRight == false)
+        {
+            FlipPlayer();
+        }
+        //PHYSICS
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveX * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+    }
+
+    void Jump()
+    {
+        GetComponent<Rigidbody2D>().AddForce(Vector2.up * playerJumpPower);
+    }
+
+    void FlipPlayer()
+    {
+        facingRight = !facingRight;
+        Vector2 localScale = gameObject.transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }
+}
